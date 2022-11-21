b5207aaa4fe897195653b3d74ef7a1574896911a
Tomer du Sautoy <tomerdusautoy@gmail.com>
2022-11-21 12:32:07 +0000

    dfg

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyToken2 is ERC721 {
+contract MyToken3 is ERC721 {
     constructor() ERC721("MyToken3", "MTK") {}
 }
